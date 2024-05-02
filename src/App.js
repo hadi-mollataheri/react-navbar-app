fb86da3c946e10a2a5fb5f3192862128dc104712
Hadi <mhadimollataheri@gmail.com>
2024-05-02 09:29:02 +0330

    Create necessary files and forlder -And Remove stric mode component from index.js and css styles from App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className=' text-red-600 bg-green-200'>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className='App-header'><h1 className="text-red-500">Hi there</h1></header>
     </div>
   );
 }
 
 export default App;
+

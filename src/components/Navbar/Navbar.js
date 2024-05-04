2d60d9af0a4c89cb997707f9b01cbcde3b0f6eb6
Hadi <mhadimollataheri@gmail.com>
2024-05-04 15:52:06 +0330

    Use .map to render navbar items

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import MenuItems from './MenuItems';
-import reactLogo from '../../assets/logo-react-svgrepo-com.svg'
+import { menuItems } from './MenuItems';
+import reactLogo from '../../assets/logo-react.svg';
 
 function Navbar() {
   return (
-    <nav className='grid grid-cols-2 justify-center p-5 bg-indigo-700 text-white'>
-
-      <div id='leftSide-container' className=' justify-self-start ml-12 flex items-center'>
+    <nav className='grid grid-cols-2 justify-center p-5 bg-react-navbar h- text-white text-lg'>
+      <div
+        id='leftSide-container'
+        className=' justify-self-start ml-12 flex items-center'
+      >
         <h3 aria-label='logo' className='text-xl font-bold'>
           React
         </h3>
         <img src={reactLogo} alt='react' className=' w-6 h-6 ml-1 fill-white' />
       </div>
-
-      <MenuItems />
+      <ul className='justify-self-end mr-12 flex gap-5 items-center justify-center'>
+        {menuItems.map((item, index) => (
+          <li key={index}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'react-navbar':
+          'linear-gradient(90deg, rgb(110, 94, 254) 0%, rgba(73, 63, 252, 1) 100%)',
+      },
+    },
   },
   plugins: [],
-}
+};
+

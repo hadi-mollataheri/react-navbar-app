b2580cdccb5d4b4038ee641225a65493a208c292
Hadi <mhadimollataheri@gmail.com>
2024-05-05 12:11:17 +0330

    Update position of the elements based on the isClicked state -Create handleClicke that changes value of the isClicked -Group the left side and mobile menu icon together

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,43 +6,55 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar({ isClicked, setIsClicked }) {
-  const handleClick = () => {};
+  const handleClick = () => {
+    setIsClicked((prev) => !prev);
+  };
 
   return (
-    <nav className='grid grid-cols-2 justify-center p-5 bg-react-navbar h- text-white text-lg'>
+    <nav
+      className={
+        isClicked === false
+          ? 'flex justify-between items-center py-5 bg-react-navbar text-white text-lg'
+          : 'justify-center pt-5 bg-react-navbar text-white text-lg'
+      }
+    >
       <div
-        id='leftSide-container'
-        className=' justify-self-start ml-12 flex items-center cursor-pointer'
+        id='top-mobile-container'
+        className={isClicked ?`flex justify-between items-center pb-5` : ''}
       >
-        <h3 aria-label='logo' className='text-xl font-bold'>
-          React
-        </h3>
-        <img src={reactIcon} alt='react' className='w-6 h-6 ml-1' />
-      </div>
+        <div id='left-side-container' className='ml-12 flex items-center cursor-pointer'>
+          <h3 aria-label='logo' className='text-xl font-bold '>
+            React
+          </h3>
+          <img src={reactIcon} alt='react' className='w-6 h-6 ml-1' />
+        </div>
 
-      <div id='rightSide-container' className='justify-self-end mr-12'>
-        <div id='menu-icon' className=' cursor-pointer' onClick={handleClick}>
+        <div
+          id='menu-icon'
+          className='justify-self-end mr-12 cursor-pointer inline-block'
+          onClick={handleClick}
+        >
           {isClicked ? (
             <FontAwesomeIcon icon={faXmark} style={{ color: '#ffffff' }} />
           ) : (
             <FontAwesomeIcon icon={faBars} style={{ color: '#ffffff' }} />
           )}
         </div>
-
-        <ul
-          className={
-            !isClicked
-              ? ' flex gap-5 items-center justify-center'
-              : ''
-          }
-        >
-          {menuItems.map((item, index) => (
-            <li key={index}>
-              <a href={item.url}>{item.title}</a>
-            </li>
-          ))}
-        </ul>
       </div>
+
+      <ul
+        className={
+          isClicked === false
+            ? 'justify-self-end mr-12 flex gap-5 items-center justify-center'
+            : ' flex flex-col items-center justify-center gap-5 bg-indigo-600 w-full col-span-2 py-6 text-base'
+        }
+      >
+        {menuItems.map((item, index) => (
+          <li key={index}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }

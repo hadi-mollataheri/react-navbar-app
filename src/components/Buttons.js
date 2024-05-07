637e9d956ec5694140598485548cdf6a0c170ed6
Hadi <mhadimollataheri@gmail.com>
2024-05-07 10:47:30 +0330

    Udate the <div>s places and Add styles -Move the ul inside the navbar's div -Remove rightSide div -Use positions to bring down the <ul>s in mobile screens -Chamge sm media query with md -Add style for button and the ul and navbar-container all for adjusting the uls for small and big screens

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Button() {
   return (
-    <button className=' bg-cyan-600 rounded-md pb-1 my-4 h-8 leading-none w-4/5 sm:w-full sm:pb-0 sm:h-6 sm:px-3 sm:m-0'>
+    <button className=' bg-cyan-600 rounded-md pb-1 my-4 h-8 leading-none px-[40%] md:h-7 md:px-3 md:m-0 md:leading-normal md:mt-1 md:pb-2'>
       sign up
     </button>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,45 +17,55 @@ function Navbar() {
     <nav className='bg-react-navbar text-white'>
       <div
         id='navbar-container'
-        className='flex justify-between items-center py-5 px-10'
+        className='flex justify-between items-center py-5 px-10 relative h-20'
       >
+
         <div
-          id='leftSide-container'
+          id='logo-container'
           className='font-bold flex justify-center items-center'
         >
           <h2 className='mr-1 text-2xl align-middle'>React</h2>
           <img src={reactIcon} alt='reactJs-icon' className='w-6 h-6' />
         </div>
-        <div id='rightSide-container'>
-          <div id='mobile-icons' className='sm:hidden'>
-            <button onClick={handleIconClick}>
-              {isClicked ? (
-                <FontAwesomeIcon
-                  icon={faXmark}
-                  size='lg'
-                  style={{ color: '#ffffff,' }}
-                />
-              ) : (
-                <FontAwesomeIcon
-                  icon={faBars}
-                  size='lg'
-                  style={{ color: '#ffffff,' }}
-                />
-              )}
-            </button>
-          </div>
+
+        <div id='mobile-icons' className='md:hidden'>
+          <button onClick={handleIconClick}>
+            {isClicked ? (
+              <FontAwesomeIcon
+                icon={faXmark}
+                size='lg'
+                style={{ color: '#ffffff,' }}
+              />
+            ) : (
+              <FontAwesomeIcon
+                icon={faBars}
+                size='lg'
+                style={{ color: '#ffffff,' }}
+              />
+            )}
+          </button>
         </div>
+
+        <ul
+          className={` ${
+            isClicked
+              ? 'flex flex-col items-center justify-center bg-indigo-600 absolute top-20 left-0 w-full'
+              : 'hidden'
+          } md:flex md:justify-center md:items-center md:gap-8 md:static`}
+        >
+          {menuItems.map((item, index) => {
+            return (
+              <li key={index} className='my-3 md:my-0'>
+                <a key={index} href={item.url}>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <Button />
+        </ul>
+
       </div>
-      <ul className={` ${isClicked ? 'flex flex-col items-center justify-center bg-indigo-600' : 'hidden'} sm:flex sm:justify-between`}>
-        {menuItems.map((item, index) => {
-          return (
-            <li key={index} className='my-3'>
-              <a key={index} href={item.url}>{item.title}</a>
-            </li>
-          );
-        })}
-        <Button />
-      </ul>
     </nav>
   );
 }
